b3180a85995bd155e11323fd4a9bc454153d1e84
sisko <siskogeorge@gmail.com>
2019-05-12 20:05:58 +0300

    app completed

--- src/app/services/api-services/drivers-list.service.ts
+++ src/app/services/api-services/drivers-list.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
-import { IDrivers } from 'src/app/models/drivers';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +11,7 @@ export class DriversListService {
 
   constructor(private http: HttpClient) {}
 
-  // getDrivers(): Observable<IDrivers[]> {
-  //   return this.http.get<IDrivers[]>(this.driversUrl);
-  // }
-  getDrivers() {
-    return this.http.get(this.driversUrl);
+  getDrivers(): Observable<any[]> {
+    return this.http.get<any[]>(this.driversUrl);
   }
 }
